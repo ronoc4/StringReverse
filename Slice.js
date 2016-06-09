51d2989dc18f37c9b5aff0886eddd86fa4e35f4f
conor <conorbudge555@gmail.com>
2016-06-09 14:51:49 -0500

    Slice methods for Array

--- Slice.js
+++ Slice.js
@@ -7,3 +7,15 @@ var str = "This is a string to slice";
 var finalSlice = str.slice(3, -2);
 
 console.log(finalSlice);
+
+
+function destroyer(arr) {
+    // Remove all the values
+    var args = Array.prototype.slice.call(arguments);
+
+    args.splice(0,1);
+
+    return args;
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
